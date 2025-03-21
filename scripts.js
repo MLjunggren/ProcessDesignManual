5b650fa58700d783bf28c0d28d9cd3af4c9c858a
MLjunggren <ljunggren.m@gmail.com>
2025-03-21 14:11:38 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const sections = document.querySelectorAll('section');
+    sections.forEach(section => {
+        const header = section.querySelector('h2');
+        header.addEventListener('click', () => {
+            section.classList.toggle('collapsed');
+        });
+    });
+});
